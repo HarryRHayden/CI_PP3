de334b32e217b5671af84dc8f163376363c3acef
Harry Hayden <h_man1992@hotmail.com>
2022-06-03 17:25:16 +0000

    add comments

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import time, random
 
-
+# Lists for potential words to guess
 boys_names = ['Luke', 'Harry', 'Edward', 'William', 'Lewis', 'Gregory']
-girls_names = ['Georgina', 'Elizabeth', 'Lucy', 'Louise', 'Ellie']
+girl_names = ['Georgina', 'Elizabeth', 'Lucy', 'Louise', 'Ellie']
+
+# Empty list for users guesses
+user_guesses = []
+user_guess_list = []
+
