40c5a9121756826e0ed4e3742cb9876f9197b2b7
Harry Hayden <h_man1992@hotmail.com>
2022-06-03 17:37:54 +0000

    add potential words into dictionary

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
 import time, random
 
 # Lists for potential words to guess
-boys_names = ['Luke', 'Harry', 'Edward', 'William', 'Lewis', 'Gregory']
-girl_names = ['Georgina', 'Elizabeth', 'Lucy', 'Louise', 'Ellie']
+gender_guess = {
+    'boys_names' : ['Luke', 'Harry', 'Edward', 'William', 'Lewis', 'Gregory'],
+    'girl_names' : ['Georgina', 'Elizabeth', 'Lucy', 'Louise', 'Ellie']
+}
 
 # Empty list for users guesses
 user_guesses = []
 # How many guesses the user gets
 GUESS_COUNT = 7
+
+def 
