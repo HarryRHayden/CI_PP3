742ea08affc07501c63a818b352fc221239c1ea3
Harry Hayden <h_man1992@hotmail.com>
2022-06-06 07:30:16 +0000

    add print statement for user choice

--- run.py
+++ run.py
@@ -12,7 +12,14 @@ user_guesses = []
 GUESS_COUNT = 7
 
 
-def choose_list(gender_guess):
+def choose_list(gender):
     """
     Loop until player has chosen a list to guess from
     """
+    choose_gender = input(
+        "Would you like to guess a boy's or girl's name?"
+    )
+    print(choose_gender)
+
+
+choose_list(gender_guess)
